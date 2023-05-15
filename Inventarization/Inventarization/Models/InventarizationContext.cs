04a5ef1f62ab55740cc47edbb37d7b79a7a03cb8
hoshiboychick <scherbodenis@gmail.com>
2023-05-15 15:10:27 +0300

    Login Window layout

---
+++ Inventarization/Inventarization/Models/InventarizationContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventarization.Models;
+
+public partial class InventarizationContext : DbContext
+{
+    public InventarizationContext()
+    {
+    }
+
+    public InventarizationContext(DbContextOptions<InventarizationContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["ConnectionLocalDb"].ToString());
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Product__3214EC07CE506E22");
+
+            entity.ToTable("Product");
+
+            entity.HasIndex(e => e.ArticleNumber, "UQ__Product__3C99114275B3F94B").IsUnique();
+
+            entity.Property(e => e.ArticleNumber).HasMaxLength(100);
+            entity.Property(e => e.Cost).HasColumnType("decimal(19, 4)");
+            entity.Property(e => e.Photo).IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Role__3214EC074CAF4171");
+
+            entity.ToTable("Role");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__User__3214EC07B51D88D6");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Patronymic).HasMaxLength(100);
+            entity.Property(e => e.Surname).HasMaxLength(100);
+
+            entity.HasOne(d => d.RoleNavigation).WithMany(p => p.Users)
+                .HasForeignKey(d => d.Role)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__User__Role__267ABA7A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Inventarization/Inventarization/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inventarization.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? ArticleNumber { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Photo { get; set; } = null!;
+
+    public string Manufacturer { get; set; } = null!;
+
+    public decimal Cost { get; set; }
+
+    public byte? DiscountAmount { get; set; }
+
+    public int QuantityInStock { get; set; }
+}
