e2573f81daf5841d0c1bd909b9d54ba82ce9deb7
hoshiboychick <scherbodenis@gmail.com>
2023-05-15 14:23:59 +0300

    Intro

---
+++ mssql.sql
@@ -0,0 +1,33 @@
+create database [Inventarization]
+go
+use [Inventarization]
+go
+create table [Role]
+(
+	Id int primary key identity,
+	Name nvarchar(100) not null
+)
+go
+create table [User]
+(
+	Id int primary key identity,
+	Surname nvarchar(100) not null,
+	Name nvarchar(100) not null,
+	Patronymic nvarchar(100) not null,
+	Login nvarchar(max) not null,
+	Password nvarchar(max) not null,
+	Role int foreign key references [Role](Id) not null
+)
+go
+create table [Product]
+(
+	Id int primary key identity,
+	ArticleNumber nvarchar(100) unique,
+	Name nvarchar(max) not null,
+	Description nvarchar(max) not null,
+	Photo varchar(max) not null,
+	Manufacturer nvarchar(max) not null,
+	Cost decimal(19,4) not null,
+    	DiscountAmount tinyint null,
+	QuantityInStock int not null
+)
